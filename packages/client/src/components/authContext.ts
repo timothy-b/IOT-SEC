f4b2fc3eaee88ab67bcfd12cb953b975428835e4
bryankenote <brykenote@gmail.com>
2022-02-23 17:17:52 -0800

    fix context type

--- packages/client/src/components/authContext.ts
+++ packages/client/src/components/authContext.ts
@@ -1,2 +1,5 @@
 import { createContext } from 'preact';
-export default createContext({ authHeader: '', setAuthHeader: () => {} });
+export default createContext<{
+	authHeader: string;
+	setAuthHeader: (authentication: string) => void;
+}>({ authHeader: '', setAuthHeader: () => {} });
