7290bb523f6bbb6d8ef67642d0b6a4bc7316a1a3
timothy-b <tim@timothy-b.com>
2024-11-03 01:13:31 -0800

    Add start of new vite-based client.

---
+++ packages/client2/src/components/Header.test.tsx
@@ -0,0 +1,11 @@
+import { Header } from './Header';
+// See: https://github.com/preactjs/enzyme-adapter-preact-pure
+import { shallow } from 'enzyme';
+
+describe('Initial Test of the Header', () => {
+	test('Header renders 2 nav items', () => {
+		const context = shallow(<Header />);
+		expect(context.find('h1').text()).toBe('IOT-SEC');
+		expect(context.find('a').length).toBe(2);
+	});
+});
---
+++ packages/client2/src/components/Header.tsx
@@ -0,0 +1,26 @@
+import Preact from 'preact';
+import { useLocation } from 'preact-iso';
+
+interface HeaderProps {
+	disableLinks?: boolean;
+}
+
+export const Header: Preact.FunctionalComponent<HeaderProps> = ({ disableLinks }) => {
+	const { url } = useLocation();
+
+	return (
+		<header>
+			<h1>IOT-SEC</h1>
+			{disableLinks ? null : (
+				<nav>
+					<a href="/" class={url == '/' && 'active'}>
+						Status
+					</a>
+					<a href="/config" class={url == '/config' && 'active'}>
+						Config
+					</a>
+				</nav>
+			)}
+		</header>
+	);
+}
